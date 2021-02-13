52286ec59ee3e846465cc16886056690a4bdfcfa
Florian Zimmermeister <flozi00.fz@gmail.com>
2021-02-13 13:18:56 +0100

    improve

--- app/src/main/java/io/aware/coala/assistant.java
+++ app/src/main/java/io/aware/coala/assistant.java
@@ -37,6 +37,7 @@ public class assistant extends Activity {
     public Button helpButton;
     public Uri mymp3 = null;
     public String mp3;
+    public boolean playSound = true;
     public MediaPlayer mediaPlayer = new MediaPlayer();
     public boolean alarm_bool, api_bool, sms_bool, call_bool;
 
@@ -73,6 +74,25 @@ public class assistant extends Activity {
             }
         });
 
+        try {
+            if(mymp3 != null){
+                try {
+                    mediaPlayer.setDataSource(wakeContext, Uri.parse(mp3));
+                } catch(Exception e){
+                    e.printStackTrace();
+                    AssetFileDescriptor descriptor = wakeContext.getAssets().openFd("alarm.mp3");
+                    mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+                    descriptor.close();
+                }
+            } else {
+                AssetFileDescriptor descriptor = wakeContext.getAssets().openFd("alarm.mp3");
+                mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+                descriptor.close();
+            }
+        } catch(Exception e){
+            playSound = false;
+        }
+
     }
 
     public void callAPI(){
@@ -102,50 +122,37 @@ public class assistant extends Activity {
     }
 
     public void playAlarm(){
-        try {
-            if(mymp3 != null){
+        if(playSound){
+            try {
+                AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+
                 try {
-                    mediaPlayer.setDataSource(wakeContext, Uri.parse(mp3));
+                    mediaPlayer.prepare();
                 } catch(Exception e){
                     e.printStackTrace();
-                    AssetFileDescriptor descriptor = wakeContext.getAssets().openFd("alarm.mp3");
-                    mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
-                    descriptor.close();
                 }
-            } else {
-                AssetFileDescriptor descriptor = wakeContext.getAssets().openFd("alarm.mp3");
-                mediaPlayer.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
-                descriptor.close();
-            }
+                mediaPlayer.setVolume(1f, 1f);
+                mediaPlayer.setLooping(false);
+                mediaPlayer.start();
 
-            AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-            mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    int maxCount = 0;
+                    int count = 0; // initialise outside listener to prevent looping
 
-            try {
-                mediaPlayer.prepare();
-            } catch(Exception e){
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        if (count < maxCount) {
+                            count = count + 1;
+                            mediaPlayer.seekTo(0);
+                            mediaPlayer.start();
+                        }
+                    }
+                });
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            mediaPlayer.setVolume(1f, 1f);
-            mediaPlayer.setLooping(false);
-            mediaPlayer.start();
-
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                int maxCount = 0;
-                int count = 0; // initialise outside listener to prevent looping
-
-                @Override
-                public void onCompletion(MediaPlayer mediaPlayer) {
-                    if (count < maxCount) {
-                        count = count + 1;
-                        mediaPlayer.seekTo(0);
-                        mediaPlayer.start();
-                    }
-                }
-            });
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
